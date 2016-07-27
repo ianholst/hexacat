e68c2d55b68c75dd06d7ffe8f7dd4483c623e853
Ian Holst <ianholst@gmail.com>
2016-07-27 16:21:44 -0400

    different auto-update

--- hexacat.sh
+++ hexacat.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 # check for git update
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse @{u})
-BASE=$(git merge-base @ @{u})
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
-else
-    echo "Diverged"
-fi
 
 
 python /root/hexacat/hexacat.py
