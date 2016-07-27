8435413f9288840e1d14fa2ea65faa2fc2b207a3
Ian Holst <ianholst@gmail.com>
2016-07-27 16:42:59 -0400

    check for new commits and pull if there are any

--- hexacat.sh
+++ hexacat.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # check for git update
+git fetch
+NEW_COMMITS="$(git rev-list HEAD...origin/master --count)"
 
+if [[ $NEW_COMMITS > 0 ]]; then
+    git pull
+fi
 
 python /root/hexacat/hexacat.py
